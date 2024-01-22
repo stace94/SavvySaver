52aacb2ce472b6523b03e099560390a7758319b1
Stace <38450525+stace94@users.noreply.github.com>
2024-01-22 15:30:54 -0500

    Update registrationPage.js

--- Front-End/src/registrationPage.js
+++ Front-End/src/registrationPage.js
@@ -12,7 +12,7 @@ export default function RegisterPage() {
   const [error, setError] = useState(null);
 
     // Assign api_url for deplyoment
-    const API_URL = 'http://localhost:5000';
+    const API_URL =  process.env.URL;
     
   // Async function to handle the registration process
   async function register(e) {
