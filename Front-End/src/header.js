981bb9266a0b2961b50703f6bceb7744b09f8faf
Stace <38450525+stace94@users.noreply.github.com>
2024-01-22 16:37:50 -0500

    Update header.js

--- Front-End/src/header.js
+++ Front-End/src/header.js
@@ -1,4 +1,4 @@
-import {React, useContext } from "react";
+import {React, useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "./userContext";
 import Logo from "./Logo/savvySaver.png";
