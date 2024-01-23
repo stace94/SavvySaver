28cfb2dae5b2ddf2e9f9ae297cf48ea7d748a4ea
Stace <38450525+stace94@users.noreply.github.com>
2024-01-23 08:27:04 -0500

    Update indexPage.js

--- Front-End/src/indexPage.js
+++ Front-End/src/indexPage.js
@@ -16,7 +16,7 @@ export default function IndexPage(){
  const [data, setData] = useState(null);
 
  // Assign api_url for deplyoment
- const API_URL =  process.env.URL;
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
  console.log(API_URL);
  // useEffect to fetch user profile data when the component mounts or setUserInfo changes
  useEffect(() => {
