be772ab08d30f2fad0209e49e24f964f0c56962d
Stace <38450525+stace94@users.noreply.github.com>
2024-01-23 08:34:45 -0500

    Update budgetPage.js

--- Front-End/src/budgetPage.js
+++ Front-End/src/budgetPage.js
@@ -16,7 +16,7 @@ export default function BudgetPage() {
   const userName = userInfo?.userName || '';
 
   // Assign api_url for deplyoment
-  const API_URL =  process.env.URL;
+  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
   // useEffect hook to fetch budget data when the component mounts or userName changes
   useEffect(() => {
